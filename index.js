77a79f6d2738f2db80f88c21ef540b12d73e51f2
Duncan Finney <duncan@leveleleven.com>
2014-09-20 00:10:52 -0400

    Playing around with error handing some

--- index.js
+++ index.js
@@ -1,40 +1,52 @@
 var koa = require('koa');
 var router = require('koa-router');
+var mongoose = require('mongoose');
+var logger = require('winston');
+var controller = require('./controllers/controller');
+
 var app = koa();
+mongoose.connect('mongodb://localhost/test');
+
+//Middleware
+app.use(function *(next){
+  //error handler 
+  try {
+    yield next;
+  } catch (ex) {
+    logger.info('caught exception: ', ex.status, ex.name + ' ' + ex.message);
+    logger.info(ex.stack)
+  }
+});
+
+app.use(function *(next){
+  var start = new Date;
+  yield next;
+  var ms = new Date - start;
+  logger.info('%s %s - %s ms', this.method, this.url, ms);
+});
+
+// API hooks
 app.use(router(app));
+app.get('/', controller.get);
+app.get('/createPost', controller.createPost);
+app.get('/throwError', controller.throwError);
+app.get('/throwError2', controller.throwError2);
 
-var winston = require('winston');
+//showing how policies would be made
+// var policies = function() {
+//   return [
+//     function*(next) {
+//       logger.info('middleware1');
+//       yield next;
+//     },
+//     function*(next) {
+//       logger.info('middleware2');
+//       yield next;
+//     }
+//   ];
+// }
+// app.get('/policiesTest', policies(), controller.get);
 
-// x-response-time
 
-app.use(function *(next){
-  var start = new Date;
-  yield next;
-  var ms = new Date - start;
-  this.set('X-Response-Time', ms + 'ms');
-});
-
-// logger
-
-app.use(function *(next){
-  var start = new Date;
-  yield next;
-  var ms = new Date - start;
-  winston.info('%s %s - %s', this.method, this.url, ms);
-});
-
-app.use(function *(next) {
-	winston.info("this.req", {
-		method: this.req.method,
-		url: this.req.url,
-		headers: this.req.heaers
-	});
-	yield next;
-});
-
-// response
-app.get('/', function *(next) {
-	this.body = 'hello world!';
-})
 app.listen(3000);
-winston.info('Listening on port 3000');
+logger.info('Listening on port 3000');
